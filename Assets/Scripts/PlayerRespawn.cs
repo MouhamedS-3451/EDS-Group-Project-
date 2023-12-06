7582f3f434ac6a47d915844903ff18e381e8174c
cbaq <karel.runkel@st.ovgu.de>
2023-12-06 20:25:39 +0100

    Add shader animation to player death/respawn

--- Assets/Scripts/PlayerRespawn.cs
+++ Assets/Scripts/PlayerRespawn.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.RenderGraphModule;
 
 public class PlayerRespawn : MonoBehaviour
 {
@@ -8,8 +9,37 @@ public class PlayerRespawn : MonoBehaviour
   public int currentCheckpoint = 0;
   public float delay = 1f;
   public float respawnDelay = 1f;
+  public float dissolveSpeed = 1f;
+  private bool fadingOut = false;
+  private bool fadingIn = false;
+
+  float fade = 1f;
   [SerializeField] new GameObject camera;
 
+  public void Update()
+  {
+    if (fadingOut)
+    {
+      fade -= Time.deltaTime;
+      if (fade < 0)
+      {
+        fade = 0;
+        fadingOut = false;
+      }
+    }
+    else if (fadingIn)
+    {
+      fade += Time.deltaTime;
+      if (fade > 1)
+      {
+        fade = 1;
+        fadingIn = false;
+      }
+    }
+    Debug.Log(fade);
+    transform.Find("PlayerSpriteRenderer").GetComponent<SpriteRenderer>().material.SetFloat("_Fade", fade);
+    //GetComponentInChildren<PlayerSpriteRenderer>().material.SetFloat("_Fade", fade);
+  }
   public void Respawn()
   {
     StartCoroutine(RespawnCoroutine());
@@ -21,17 +51,19 @@ public class PlayerRespawn : MonoBehaviour
     GetComponent<Movement>().deceleration = 25f;
     GetComponent<Movement>().active = false;
     GetComponent<Jumping>().active = false;
-    
+    fadingOut = true;
+
     yield return new WaitForSeconds(delay);
 
     transform.position = respawnPoint.position;
+    fadingIn = true;
 
     yield return new WaitForSeconds(respawnDelay);
 
     GetComponent<Movement>().deceleration = deceleration;
     GetComponent<Movement>().active = true;
-    GetComponent<Jumping>().active = true;    
-    
+    GetComponent<Jumping>().active = true;
+
   }
 
   public void SetRespawnPoint(Transform point)
