460409b2eba81c1202c955366e127d48312db843
cbaq <karel.runkel@st.ovgu.de>
2023-12-16 06:36:52 +0100

    Add jump boost interactable and refactor image assets

--- Assets/Scripts/InteractableJumpBoost.cs
+++ Assets/Scripts/InteractableJumpBoost.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class InteractableJumpBoost : Interactable
 {
+  [SerializeField] GameObject player;
   [SerializeField] float jumpBoostMultiplier = 2f;
+  [SerializeField] Sprite spriteDown;
+  private Sprite spriteUp;
+
+  public void Awake()
+  {
+    spriteUp = transform.GetComponentInParent<SpriteRenderer>().sprite;
+  }
 
   public override void InRangeAction()
   {
-    GameObject.Find("Player").GetComponent<Jumping>().jumpHeightMultiplier = jumpBoostMultiplier;
+    player.GetComponent<Jumping>().jumpHeightMultiplier = jumpBoostMultiplier;
+    //transform.GetComponentInParent<SpriteRenderer>().enabled = false;
+    transform.GetComponentInParent<SpriteRenderer>().sprite = spriteDown;
   }
+
   public override void OutOfRangeAction()
   {
-    GameObject.Find("Player").GetComponent<Jumping>().jumpHeightMultiplier = 1f;
+    player.GetComponent<Jumping>().jumpHeightMultiplier = 1f;
+    transform.GetComponentInParent<SpriteRenderer>().sprite = spriteUp;
   }
 }
