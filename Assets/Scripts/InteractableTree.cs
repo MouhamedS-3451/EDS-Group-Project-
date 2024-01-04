58243654769b1b22251268f607af9ebd4435b0ef
cbaq <karel.runkel@st.ovgu.de>
2024-01-04 17:29:05 +0100

    Trees can toggle gameObjects

--- Assets/Scripts/InteractableTree.cs
+++ Assets/Scripts/InteractableTree.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class InteractableTree : Interactable
@@ -15,6 +16,9 @@ public class InteractableTree : Interactable
   private float growthMin = 0f;
   private bool isInteractable = true;
   private bool isGrowing = false;
+  [SerializeField] private bool toggleInstant = false;
+  [SerializeField] private List<GameObject> gameObjectsToToggle;
+  private List<bool> gameObjectsStatus = new List<bool>();
 
   public void Awake()
   {
@@ -34,7 +38,13 @@ public class InteractableTree : Interactable
         child.GetComponent<SpriteRenderer>().material.SetFloat("_Strength", bgStrength);
       }
     }
+
+    foreach (GameObject gameObject in gameObjectsToToggle)
+    {
+      gameObjectsStatus.Add(gameObject.activeSelf);
+    }
   }
+
   // Update updates the growth value over time
   public void Update()
   {
@@ -47,6 +57,24 @@ public class InteractableTree : Interactable
     {
       if (child.tag.CompareTo("Toggleable") == 1) continue;
       child.GetComponent<SpriteRenderer>().material.SetFloat("_Cutoff_Height", growthValue);
+    }
+
+
+    if (toggleInstant) return;
+    for (int i = 0; i < gameObjectsToToggle.Count; i++)
+    {
+      gameObjectsToToggle[i].SetActive(true);
+      try
+      {
+        Color spriteColor = gameObjectsToToggle[i].GetComponentInChildren<SpriteRenderer>(includeInactive: true).color;
+
+        if (gameObjectsStatus[i]) spriteColor.a = 1 - growthValue;
+        else spriteColor.a = growthValue;
+
+        gameObjectsToToggle[i].GetComponentInChildren<SpriteRenderer>().color = spriteColor;
+      }
+      catch { }
+
     }
   }
 
@@ -72,6 +100,13 @@ public class InteractableTree : Interactable
     cam.GetComponent<CameraFollow>().PosSmoothTime = 0.5f;
 
     while (isGrowing) yield return null;
+
+
+    for (int i = 0; i < gameObjectsToToggle.Count; i++)
+    {
+      gameObjectsToToggle[i].SetActive(!gameObjectsStatus[i]);
+    }
+
 
     // Pan camera back to player
     cam.GetComponent<CameraFollow>().target = player.transform;
